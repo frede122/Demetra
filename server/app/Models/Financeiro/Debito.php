8a433c96ebe1ed3c720257eedde04957dfbfe354
frede122 <fredemanduoliveira@hotmail.com>
2021-11-13 00:16:16 -0300

    feat: adicionado Debito Api :rocket: criado Debito Controller criado Debito Model criado Debito Migration

--- server/app/Models/Financeiro/Debito.php
+++ server/app/Models/Financeiro/Debito.php
@@ -1,49 +1,42 @@
 <?php
 
-namespace App\Models\Financeiro; 
-use App\Models\Setor;    
+namespace App\Models\Financeiro;
 
+use App\Models\Setor;  
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Debito extends Model
 {
-
-    public function __construct()
-    {
-        
-    }
-
     protected $primaryKey = 'debito_id';
 
     protected $fillable = [
-        'doc_id', 
-        'data',
+        'docs_id', 
         'fornecedor_id',
+        'data',
         'descricao',
         'quantidade', 
         'valor_unitario', 
         'setor_id', 
         'sub_categoria_id'
-        
     ];
-    
+
     public function fornecedor(){
         return $this->hasOne(Fornecedor::class, 'fornecedor_id', 'fornecedor_id');
     }
 
     
     public function subCategoria(){
-        return $this->hasOne(SubCategoria::class, 'sub_categori_id', 'sub_categoria_id');
+        return $this->hasOne(SubCategoria::class, 'sub_categoria_id', 'sub_categoria_id');
     }
 
     public function categoria(){
-        return $this->hasOneThrough(Categoria::class,  SubCategoria::class, 'categori_id', 'categori_id', 'sub_categori_id', 'sub_categorias_id');
+        return $this->hasOneThrough(Categoria::class,  SubCategoria::class, 'categoria_id', 'categoria_id', 'sub_categoria_id', 'sub_categoria_id');
     }
 
     public function doc(){
-        return $this->hasOne(Doc::class, 'doc_id', 'doc_id');
+        return $this->hasOne(Doc::class, 'docs_id', 'docs_id');
     }
+
     public function setor(){
         return $this->hasOne(Setor::class, 'setor_id', 'setor_id');
     }
